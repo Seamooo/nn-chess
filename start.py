d23af045e8ea474325b5682a4345c6d2a25789a9
Seamus Mulholland-Patterson <seamus1103@gmail.com>
2020-05-23 23:40:27 +0800

    added random agent

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, url_for, send_from_directory, request
+from flask import Flask, url_for, send_from_directory, request, jsonify
+from chess import Board
+import random
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 
@@ -13,7 +15,19 @@ def send_img(path):
 
 @app.route('/ai', methods=['POST'])
 def get_move():
-	return ''
+	data = request.json
+	if 'state' not in data:
+		abort(400)
+	try:
+		board = Board(data['state'])
+	except Exception as e:
+		abort(400)
+	moves = list(board.legal_moves)
+	if not moves:
+		abort(400)
+	move = random.choice(moves)
+	print(move)
+	return jsonify({'from':str(move)[:2],'to':str(move)[2:]})
 
 #server running on port 4444
 if __name__ == '__main__':
